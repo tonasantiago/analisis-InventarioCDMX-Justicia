a72c77e0597fc8ac67f0712a95c61d2471cff352
Tona Santiago <tona@comunidad.unam.mx>
2021-04-28 12:37:35 +0000

    Consolidation of files.New README

--- Analisis.R
+++ Analisis.R
@@ -1,3 +1,37 @@
+rm(list = ls(all = TRUE))
+# Cargar el dataset y crear la variable UPM ------------------------------
+
+jus  <- data.table::fread('Base_SPyJ.csv')
+
+library(tidyverse)
+jus <- jus %>% unite("UPM", ESTADO:AGEB, remove = FALSE)
+
+
+# Declarar el diseño de la muestra ---------------------------------------
+
+library(survey)
+design = svydesign(id=~jus$UPM,strata=~jus$REGIÓN, 
+                   weights=~jus$PONDI1, data=jus)
+
+
+# Frecuencias simples -----------------------------------------------------
+
+library(survey)
+gp1 <- as.data.frame(round(prop.table(svytable(~P52, 
+                                               design=design))*100,1))
+
+gp2 <- as.data.frame(round(prop.table(svytable(~P49, 
+                                               design=design))*100,1))
+
+gp3 <- as.data.frame(round(prop.table(svytable(~P12, 
+                                               design=design))*100,1))
+
+gp4 <- as.data.frame(round(prop.table(svytable(~P13, 
+                                               design=design))*100,1))
+
+
+
+# Crear índices -----------------------------------------------------------
 
 # Índice excepción a la ley----
 
@@ -144,7 +178,7 @@ detach(jus)
 
 # Ordenar etiquetas
 jus$cl_justicia <- factor(jus$cl_justicia,levels = c("Baja confianza","Media confianza",
-                                           "Alta confianza"))
+                                                     "Alta confianza"))
 
 # Borrar objetos innecesarios y volver a declarar el diseño de la encuesta
 rm(tranclus)
@@ -206,7 +240,7 @@ jus$cl_resoluciones[tcl_resoluciones == centrinos2$rowid[3]] <- "Alta confianza"
 detach(jus)
 
 jus$cl_resoluciones<- factor(jus$cl_resoluciones,levels = c("Baja confianza","Media confianza",
-                                                     "Alta confianza"))
+                                                            "Alta confianza"))
 
 # Borrar objetos innecesarios y volver a declarar el diseño de la encuesta
 rm(tranclus)
@@ -261,7 +295,7 @@ detach(jus)
 # Ordenar etiquetas
 jus$cl_conflicto <- factor(jus$cl_conflicto,levels = c("Baja autocomposición",
                                                        "Media autocomposición",
-                                                     "Alta autocomposición"))
+                                                       "Alta autocomposición"))
 
 # Borrar objetos innecesarios y volver a declarar el diseño de la encuesta
 rm(tranclus)
@@ -276,4 +310,80 @@ design = svydesign(id=~jus$UPM,strata=~jus$REGIÓN,
 #Validación 
 jus%>%group_by(P13)%>%summarize(puntuacion=mean(conflicto))
 vtree::vtree(jus, "conflicto cl_conflicto")
+
+
+# Tablas cruzadas índices -------------------------------------------------
+
+
+# Reconvertir Ingreso -----------------------------------------------------
+
+attach(jus)
+jus$S111B[S11==" De 0 a 2 SM ($0 hasta $6,161 al mes) "] <- " De 0 a 2 SM ($0 hasta $6,161 al mes) "
+jus$S111B[S11==" De 2 a 4 SM De ($6,162 hasta $12,322 al mes) "] <- " De 2 a 4 SM De ($6,162 hasta $12,322 al mes) "
+jus$S111B[S11==" De 4 a 6 SM De ($12,323 hasta $18,482 al mes) "] <- " De 4 a 6 SM De ($12,323 hasta $18,482 al mes) "
+jus$S111B[S11==" De 6 a 8 SM De ($18,483 hasta $24,643 al mes) "] <- " Más de 6 SM $18,483 o más mensuales) "
+jus$S111B[S11==" De 8 a 10 SM De ($24,644 hasta $30,804 al mes) "] <- " Más de 6 SM $18,483 o más mensuales) "
+jus$S111B[S11==" De 10 a 12 SM De ($30,805 hasta $36,965 al mes) "] <- " Más de 6 SM $18,483 o más mensuales) "
+jus$S111B[S11==" De 12 a 14 SM De ($36,966 hasta $43,126 al mes) "] <- " Más de 6 SM $18,483 o más mensuales) "
+jus$S111B[S11==" De 14 a 16 SM De ($43,127 hasta $49,286 al mes) "] <- " Más de 6 SM $18,483 o más mensuales) "
+jus$S111B[S11==" De 16 a 18 SM De ($49,287 hasta $55,447 al mes) "] <- " Más de 6 SM $18,483 o más mensuales) "
+jus$S111B[S11==" De 18 a 20 SM De ($55,448 hasta $61,608 al mes) "] <- " Más de 6 SM $18,483 o más mensuales) "
+jus$S111B[S11==" Más de 20 SM ($61,609 o más mensuales) "] <- " Más de 6 SM $18,483 o más mensuales) "
+jus$S111B[S11==" NS "] <- NA
+jus$S111B[S11==" NC "] <- NA
+detach(jus)
+
+rm(design)
+design = svydesign(id=~jus$UPM,strata=~jus$REGIÓN, 
+                   weights=~jus$PONDI1, data=jus)
+
+
+# Excpeción a la ley --------------------------------------------------------
+library(pander)
+pander(round(prop.table(svytable(~cl_ley, design=design))*100,1))
+pander((na.omit(prop.table(na.omit(svytable(~SEXO+cl_ley, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~EDAD+cl_ley, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~ESCOLARIDAD+cl_ley, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~S111B+cl_ley, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~P49+cl_ley, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~P9+cl_ley, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~cl_conflicto+cl_ley, design)),1)))*100)
+
+
+# Confianza operadores---------------------------------------------------------------
+
+library(pander)
+pander(round(prop.table(svytable(~cl_justicia, design=design))*100,1))
+pander((na.omit(prop.table(na.omit(svytable(~SEXO+cl_justicia, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~EDAD+cl_justicia, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~ESCOLARIDAD+cl_justicia, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~S111B+cl_justicia, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~P49+cl_justicia, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~P9+cl_justicia, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~cl_conflicto+cl_justicia, design)),1)))*100)
+
+
+# Resoluciones ------------------------------------------------------------
+
+library(pander)
+pander(round(prop.table(svytable(~cl_resoluciones, design=design))*100,1))
+pander((na.omit(prop.table(na.omit(svytable(~SEXO+cl_resoluciones, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~EDAD+cl_resoluciones, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~ESCOLARIDAD+cl_resoluciones, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~S111B+cl_resoluciones, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~P49+cl_resoluciones, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~P9+cl_resoluciones, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~cl_conflicto+cl_resoluciones, design)),1)))*100)
+
+# Autocomposición ---------------------------------------------------------
+
+library(pander)
+pander(round(prop.table(svytable(~cl_conflicto, design=design))*100,1))
+pander((na.omit(prop.table(na.omit(svytable(~SEXO+cl_conflicto, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~EDAD+cl_conflicto, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~ESCOLARIDAD+cl_conflicto, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~S111B+cl_conflicto, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~P49+cl_conflicto, design)),1)))*100)
+pander((na.omit(prop.table(na.omit(svytable(~P9+cl_conflicto, design)),1)))*100)
+
 
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# analisis-inventarioCDMX-justicia
+# Análisis de la Encuesta sobre Seguridad y Justicia del proyecto "Invetario CDMX. Presente y futuro de su gente"
+
+Este proyecto contiene el script necesario para realizar los análisis del documento *La influencia de las percepciones de los habitantes de la CDMX en el uso y aceptación social de los mecanismos alternativos de solución de controversias en materia penal* de Daniel H. Gaona.
+
+El proyecto busca relacionar las percepeciones de los habitantes de la Ciudad de México sobre los nuevos mecanismos de impartición de justicia.
+
+
+El script realiza las siguientes operaciones: 
+
+1. Con el conjunto de datos proporcionados, junto con el diseño muestral, se crea una nueva variable ("UPM") que servirá para declarar el diseño muestral.
+2. Al ser datos que provienen de un diseño muestral se necesita declara dicho diseño para hacer inferencias válidas, para eso su usa el paquete  ```survey 
+```.
+3. Se guardan algunas frecuencias simples de interés para el estudio
+4. Se crean algunos índices de interés para el estudio
+5. Se cruzan algunos índices con algunas otras preguntas del cuestionario de la encuesta así como con algunas datos sociodemográficos 
+
+Los resultados finales, así como al discusión teórica que sustenta estos análisis puede encontrarse en: *La influencia de las percepciones de los habitantes de la CDMX en el uso y aceptación social de los mecanismos alternativos de solución de controversias en materia penal* / tesis que para optar por el grado de Maestro en Derecho, presenta Eduardo Daniel Hernández Gaona ; tutor principal de tesis Carlos Silva Forné --  2021
+
